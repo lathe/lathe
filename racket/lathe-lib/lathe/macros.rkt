38a1cb598db578a8cee69cf95050acd8a74b0bd4
Ross Angle <rokitna@hotmail.com>
2017-05-03 08:36:08 -0700

    In macros.rkt, bring the original unit tests up to date, and uncomment them.

--- racket/lathe-lib/lathe/macros.rkt
+++ racket/lathe-lib/lathe/macros.rkt
@@ -151,10 +151,22 @@
 
 
 (struct op-nest () #:prefab)
+(define (env-with-op-nest done bind env) #/lambda (env2 op body)
+#/match op
+  [(op-nest)
+  #/bind (qexp-bind-esc done bind body #/lambda (leaf)
+         #/bind (interpret done bind env2 leaf) #/match-lambda
+           [(esc i leaf) #/done #/esc i #/qexp-literal #/done leaf]
+           [(ret leaf) #/done #/ret #/qexp-literal #/done leaf])
+  #/match-lambda
+    [(esc i body) #/done #/esc i #/qexp-call (op-nest) body]
+    [(ret body) #/done #/ret #/qexp-call (op-nest) body]]
+  [op (env env2 op body)])
+
 (struct op-double () #:prefab)
 
 (w- done monad-maybe-done bind monad-maybe-bind
-  env (core-env done bind)
+  env (env-with-op-nest done bind #/core-env done bind)
   env (lambda (env2 op body) #/match op
         
         [(op-nest)
@@ -220,100 +232,82 @@
     #/qexp-literal #/done 1)))
 
 
-; TODO: Bring the following code up to date.
-#|
-
 (struct lists-and-rest (lists rest) #:prefab)
 
-(define (list->flat-qexp eof lst) #/match lst
-  [(list) #/qexp-literal #/monad-maybe-done eof]
+(define (list->flat-qexp done eof lst) #/match lst
+  [(list) #/qexp-literal #/done #/qexp-literal #/done eof]
   [(cons first rest)
-  #/qexp-call (qexp-literal #/monad-maybe-done first)
-  #/qexp-literal #/monad-maybe-done #/list->flat-qexp eof rest])
+  #/qexp-call first #/qexp-literal #/done
+  #/list->flat-qexp done eof rest])
 
-(define (flat-qexp->nested-qexp flat-qexp)
+(define (flat-qexp->nested-qexp done bind flat-qexp)
   (some-val #/run-monad-maybe
   #/interpret monad-maybe-done monad-maybe-bind
-    (lambda (env op done bind body) #/match op
-      [(op-nest)
-      #/done #/qexp-call (qexp-literal #/done #/op-nest) #/qexp-literal
-      ; TODO
-      #/done body];#/qexp-mappend body done bind #/lambda (qexp)
-      ;  (interpret done bind env qexp)]
-      [op (core-env env op done bind body)])
+    (env-with-op-nest done bind #/core-env done bind)
     flat-qexp))
 
 (define (nested-qexp->lists-and-rest qexp) #/match qexp
   [(qexp-literal rest) (lists-and-rest (list) rest)]
   [ (qexp-call op body) #/match op
-    [ (qexp-literal op) #/match (run-monad-maybe op)
-      [ (some op) #/match op
-        [(op-nest)
-        #/match (nested-qexp->lists-and-rest body)
-          [(lists-and-rest lists1 rest)
-          #/match (run-monad-maybe rest)
-            [(some rest)
-            #/match (nested-qexp->lists-and-rest rest)
-              [ (lists-and-rest lists2 rest)
-                (lists-and-rest (cons lists1 lists2) rest)]
-              [lists-and-rest (error "Internal error: Expected a nested-qexp->lists-and-rest result that was a lists-and-rest")]]
-            [rest (error "Expected a rest that was a some")]]
+    [(op-nest)
+    #/match (nested-qexp->lists-and-rest body)
+      [(lists-and-rest lists1 rest)
+      #/match (run-monad-maybe rest)
+        [(some rest)
+        #/match (nested-qexp->lists-and-rest rest)
+          [ (lists-and-rest lists2 rest)
+            (lists-and-rest (cons lists1 lists2) rest)]
           [lists-and-rest (error "Internal error: Expected a nested-qexp->lists-and-rest result that was a lists-and-rest")]]
-        ; NOTE: While it could make sense to support other operators
-        ; like (op-id), that would make the representation
-        ; non-canonical.
-        [op (error "Expected an op that was an op-nest")]]
-      [op (error "Expected an op that was a some")]]
-    ; NOTE: While it could make sense to support an operator that was
-    ; obtained by a call to a simple operator like (op-id), that would
-    ; make the representation non-canonical.
-    [op (error "Expected an op that was a qexp-literal")]]
-  [qexp (display "nested-qexp->lists-and-rest ") (writeln qexp)
+        [rest (error "Expected a rest that was a some")]]
+      [lists-and-rest (error "Internal error: Expected a nested-qexp->lists-and-rest result that was a lists-and-rest")]]
+    ; NOTE: While it could make sense to support other operators
+    ; like (op-id), that would make the representation
+    ; non-canonical.
+    [op (error "Expected an op that was an op-nest")]]
+  [qexp
     (error "Expected a qexp that was a qexp-literal or a qexp-call")])
-
-
 
 (struct dummy-eof () #:prefab)
 
 (define list-example #/list
-  (op-open-paren)
-  (op-open-paren)
+  (op-open-paren #/op-nest)
+  (op-open-paren #/op-nest)
   (op-close-paren)
-;  (op-open-paren)
-;  (op-close-paren)
+  (op-open-paren #/op-nest)
+  (op-close-paren)
   (op-close-paren))
 
 (define flat-example
-  (qexp-call (qexp-literal #/monad-maybe-done #/op-open-paren) #/qexp-literal #/monad-maybe-done
-  #/qexp-call (qexp-literal #/monad-maybe-done #/op-open-paren) #/qexp-literal #/monad-maybe-done
-  #/qexp-call (qexp-literal #/monad-maybe-done #/op-close-paren) #/qexp-literal #/monad-maybe-done
-;  #/qexp-call (qexp-literal #/monad-maybe-done #/op-open-paren) #/qexp-literal #/monad-maybe-done
-;  #/qexp-call (qexp-literal #/monad-maybe-done #/op-close-paren) #/qexp-literal #/monad-maybe-done
-  #/qexp-call (qexp-literal #/monad-maybe-done #/op-close-paren) #/qexp-literal #/monad-maybe-done
-  #/qexp-literal #/monad-maybe-done #/dummy-eof))
-
-(define nested-example
-  (qexp-call (qexp-literal #/monad-maybe-done #/op-nest)
-  #/(lambda (it)
-      (qexp-call (qexp-literal #/monad-maybe-done #/op-nest)
-      #/qexp-literal #/monad-maybe-done
-;      #/qexp-call (qexp-literal #/monad-maybe-done #/op-nest)
-;      #/qexp-literal #/monad-maybe-done
-        it))
+  (qexp-call (op-open-paren #/op-nest) #/qexp-literal #/monad-maybe-done
+  #/qexp-call (op-open-paren #/op-nest) #/qexp-literal #/monad-maybe-done
+  #/qexp-call (op-close-paren) #/qexp-literal #/monad-maybe-done
+  #/qexp-call (op-open-paren #/op-nest) #/qexp-literal #/monad-maybe-done
+  #/qexp-call (op-close-paren) #/qexp-literal #/monad-maybe-done
+  #/qexp-call (op-close-paren) #/qexp-literal #/monad-maybe-done
   #/qexp-literal #/monad-maybe-done
   #/qexp-literal #/monad-maybe-done #/dummy-eof))
 
-;(define sexp-example (list (list) (list)))
-(define sexp-example (list (list)))
-;(define sexp-example (list))
+(define nested-example
+  (qexp-call (op-nest)
+  #/(lambda (it)
+      (qexp-call (op-nest) #/qexp-literal #/monad-maybe-done
+      #/qexp-call (op-nest) #/qexp-literal #/monad-maybe-done it))
+  #/qexp-literal #/monad-maybe-done
+  #/qexp-literal #/monad-maybe-done #/dummy-eof))
 
-#;(equal? (list->flat-qexp (dummy-eof) list-example) flat-example)
-(equal? (flat-qexp->nested-qexp flat-example) nested-example)
-#;(equal? (nested-qexp->lists-and-rest nested-example)
+(define sexp-example (list (list) (list)))
+
+(equal? (list->flat-qexp monad-maybe-done (dummy-eof) list-example)
+  flat-example)
+(equal?
+  (flat-qexp->nested-qexp monad-maybe-done monad-maybe-bind
+    flat-example)
+  (ret nested-example))
+(equal? (nested-qexp->lists-and-rest nested-example)
   (lists-and-rest (list sexp-example) (monad-maybe-done #/dummy-eof)))
 
-#;(equal?
-  (nested-qexp->lists-and-rest #/flat-qexp->nested-qexp
-  #/list->flat-qexp (dummy-eof) list-example)
+(equal?
+  (nested-qexp->lists-and-rest #/ret-val
+  #/flat-qexp->nested-qexp monad-maybe-done monad-maybe-bind
+  #/list->flat-qexp monad-maybe-done (dummy-eof) list-example)
   (lists-and-rest (list sexp-example) (monad-maybe-done #/dummy-eof)))
-|#
