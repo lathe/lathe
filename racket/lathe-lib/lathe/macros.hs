dcfe2135baa207e0bf6d0ac3ed479e7cb73049bf
Ross Angle <rokitna@hotmail.com>
2017-05-07 04:31:42 -0700

    In macros.hs, find a functor that can be iterated for progressively deeper finite-degree quasiquotations.

--- racket/lathe-lib/lathe/macros.hs
+++ racket/lathe-lib/lathe/macros.hs
@@ -6,118 +6,10 @@
 
 {-# LANGUAGE RankNTypes #-}
 
--- {-# LANGUAGE ExistentialQuantification #-}
-
 import Data.Void
+import Data.Functor.Identity
+import Data.Functor.Compose
 
-{-
-
-class (Monad atree, Monad btree) =>
-  Interpretable env esc err op atree aleaf btree bleaf where
-  
-  toMacroCall ::
-    atree aleaf' -> Either aleaf' (op, atree (atree aleaf'))
-  interpretLeaf :: aleaf -> bleaf
-  idOp ::
-    env -> atree (atree aleaf) ->
-      btree (Either bleaf (esc, atree aleaf))
-  unwrapEnv ::
-    env -> env -> op -> atree (atree aleaf) ->
-      btree (Either bleaf (esc, atree aleaf))
-  wrapEnv ::
-    (env -> op -> atree (atree aleaf) ->
-      btree (Either bleaf (esc, atree aleaf))
-    ) ->
-      env
-  
-  interpret ::
-    env -> atree aleaf ->
-      btree (Either bleaf (esc, atree aleaf))
-  interpret env expr = case toMacroCall expr of
-    Left aleaf -> return (Left (interpretLeaf aleaf))
-    Right (op, body) -> unwrapEnv env env op body
-  
-  unwrapEnv (wrapEnv unwrappedEnv) ~ unwrappedEnv
-  wrapEnv (unwrapEnv wrappedEnv) ~ wrappedEnv
-  idOp env body ~ case toMacroCall body of
-    Left following -> case toMacroCall following of
-      Left leaf -> return (Left (interpretLeaf leaf))
-      Right (op, following')
-        return (Right (esc, following'))
-    Right _ -> TODO
-
--}
-
-
-{-
-
-data Expr m a lit = Literal lit | Call a (m (Expr m a lit))
-
-exprLiteralDone :: (Monad m) => lit -> Expr m a lit
-exprLiteralDone result = Literal result
-exprLiteralBind :: (Monad m) => Expr m a lita -> (lita -> Expr m a litb) -> Expr m a litb
-exprLiteralBind effects next = case effects of
-  Literal lit -> next lit
-  Call op body -> Call op (fmap (\body' -> exprLiteralBind body' next) body)
-instance (Monad m) => Monad (Expr m a) where
-  return = exprLiteralDone
-  (>>=) = exprLiteralBind
-
-exprOpDone :: (Monad m) => a -> Expr m a ()
-exprOpDone result = Call result (return (Literal ()))
-exprOpBind :: (Monad m) => Expr m a () -> (a -> Expr m b ()) -> Expr m b ()
-exprOpBind effects next = case effects of
-  Literal () -> Literal ()
-  Call op body -> 
-  Call op body -> Call op (body >>= \body' -> exprLiteralBind body' next)
-
-
-
-
-data Expr m a lit = Literal lit | Call a (Expr m a (m (Expr m a lit)))
-data MExpr m a lit = MExpr (m (Expr m a lit))
-
-deMExpr :: MExpr m a lit -> m (Expr m a lit)
-deMExpr (MExpr internal) = internal
-
-exprLiteralDone :: (Monad m) => lit -> MExpr m a lit
-exprLiteralDone x = MExpr (return (Literal x))
-exprLiteralBind :: (Monad m) => MExpr m a lita -> (lita -> MExpr m a litb) -> MExpr m a litb
-exprLiteralBind (MExpr effects) next = MExpr $ effects >>= \effects' -> deMExpr $ case effects' of
-  Literal lit -> next lit
-  Call op body ->
-    exprLiteralBind (MExpr (return body)) $ \leaf ->
-    exprLiteralBind (MExpr leaf) next
-instance (Monad m) => Monad (MExpr m a) where
-  return = exprLiteralDone
-  (>>=) = exprLiteralBind
-
--}
-
-{-
-exprOpDone :: (Monad m) => a -> (forall lit. lit -> Expr m a lit)
-exprOpDone x = \lit -> Call op (Literal (return (Literal lit)))
-exprOpBind ::
-  (Monad m) =>
-  (forall lit. lit -> Expr m a lit) ->
-  (a -> (forall lit. lit -> Expr m b lit)) ->
-    (forall lit. lit -> Expr m b lit)
-exprOpBind effects next = \lit -> case effects lit of
-  Literal lit' => Literal lit'
-  Call op body =>
-    -- continuation is type (Expr m b lit)
-    -- op is type a
-    -- next is type (a -> (forall lit. lit -> Expr m b lit))
-    -- body is type (Expr m a (Expr m a lit))
-exprOpBind :: (Monad m) => Expr m a Unit -> (a -> Expr m b Unit) -> Expr m b Unit
-exprOpBind effects next = case effects of
-  Literal lit => Literal lit
-  Call op body =>
-    -- continuation is type (Expr m b lit)
-    -- op is type a
-    -- next is type (a -> (forall lit. Expr m b lit))
-    -- body is type (Expr m a (Expr m a lit))
--}
 
 data Expr m a lit = Literal lit | Call a (Expr m a (m (Expr m a lit)))
 
@@ -212,13 +104,15 @@ enforceLiteral expr = case expr of
     Call "Encountered an operator where none was expected" $
       exprMapLiteral (fmap enforceLiteral) $ enforceLiteral body
 
-data ExprAsFunctor m lit a = ExprAsFunctor (Expr m a lit)
-instance (Functor m) => Functor (ExprAsFunctor m lit) where
+data ExprAsFunctor lit m a =
+  ExprAsFunctor {deExprAsFunctor :: (Expr m a lit)}
+
+instance (Functor m) => Functor (ExprAsFunctor lit m) where
   fmap func (ExprAsFunctor expr) = ExprAsFunctor $ exprMapOp func expr
 
 exprAsFunctorGetOneAndOnly ::
   (forall a. m a -> Maybe a) ->
-    (forall a. ExprAsFunctor m lit a -> Maybe a)
+    (forall a. ExprAsFunctor lit m a -> Maybe a)
 exprAsFunctorGetOneAndOnly getOneAndOnly (ExprAsFunctor expr) =
   case expr of
     Literal lit -> Nothing
@@ -229,6 +123,237 @@ exprAsFunctorGetOneAndOnly getOneAndOnly (ExprAsFunctor expr) =
           Call op''' body''' -> Nothing
           Literal lit -> Just op
 
-instance (SyntaxMaterial m) => SyntaxMaterial (ExprAsFunctor m lit)
-where
+instance (SyntaxMaterial m) => SyntaxMaterial (ExprAsFunctor lit m)
+  where
   getOneAndOnly = exprAsFunctorGetOneAndOnly getOneAndOnly
+
+instance SyntaxMaterial Maybe where
+  getOneAndOnly = id
+
+listToExpr :: [a] -> ExprAsFunctor () Maybe a
+listToExpr list = ExprAsFunctor $ case list of
+  [] -> Literal ()
+  x:xs -> Call x $ Literal $ Just $ deExprAsFunctor $ listToExpr xs
+
+listFromExpr :: ExprAsFunctor () Maybe a -> Either String [a]
+listFromExpr (ExprAsFunctor expr) = case expr of
+  Literal () -> Right []
+  Call x body -> case body of
+    Call op' body' ->
+      Left "Encountered content nested within a list element"
+    Literal body' -> case body' of
+      Nothing -> Left "Encountered no syntax following a list element"
+      Just body'' ->
+        (listFromExpr $ ExprAsFunctor body'') >>= \xs -> return (x:xs)
+
+data SExpr a = SExprLiteral a | SExprList [SExpr a]
+  deriving Show
+
+sExprToExpr ::
+  SExpr a -> ExprAsFunctor () (ExprAsFunctor () Maybe) (Maybe a)
+sExprToExpr sExpr = ExprAsFunctor $ case sExpr of
+  SExprLiteral a ->
+    Call (Just a) $ Literal $ ExprAsFunctor $ Literal ()
+  SExprList list ->
+    Call Nothing $ Literal $ listToExpr $
+      fmap (deExprAsFunctor . sExprToExpr) list
+
+sExprFromExpr ::
+  ExprAsFunctor () (ExprAsFunctor () Maybe) (Maybe a) ->
+    Either String (SExpr a)
+sExprFromExpr (ExprAsFunctor expr) = case expr of
+  Literal () -> Left "Encountered no syntax"
+  Call a body -> case a of
+    Just a -> case body of
+      Call op' body' ->
+        Left "Encountered content nested within an s-expression literal's contour"
+      Literal (ExprAsFunctor body') -> case body' of
+        Call op' body' ->
+          Left "Encountered content nested within an s-expression literal"
+        Literal () -> Right (SExprLiteral a)
+    Nothing -> case body of
+      Call op' body' ->
+        Left "Encountered content nested within an s-expression list's contour"
+      Literal exprOfList -> do
+        list <- listFromExpr exprOfList
+        list' <- mapM (sExprFromExpr . ExprAsFunctor) list
+        return $ SExprList list'
+
+-- We've defined encoders and parsers for traditional lists and
+-- s-expressions, but they involve several potential error conditions,
+-- and they're not very consistent with each other, which is going to
+-- make it difficult to extrapolate to higher orders of
+-- quasiquotation.
+--
+-- Ideally there's only one error condition we need: These lists and
+-- s-expressions are distinguished by having a designated limit to the
+-- degree of quasiquotation they can do, so we want to worry about
+-- that error and no others.
+--
+-- We define `CallSExpr`, an s-expression representation which can be
+-- parsed from a particular variant of `Expr` while having only that
+-- potential error. The corresponding (nonempty) list representation
+-- is `CallList`, and they're related by a functor `ExprAsCallFunctor`
+-- which can be repeatedly applied to `Identity` to obtain higher
+-- degrees of the syntax.
+--
+--
+-- TODO: This actually suggests a drastic revision to `Expr`:
+--
+--   data Expr m a = Expr a (Maybe (m (Expr m a)))
+--
+-- That way the finite-order representations are (Const Void),
+-- (Expr (Const Void)), (Expr (Expr (Const Void))), and so on, and the
+-- infinite-order representation is (Fix Expr).
+--
+-- This almost corresponds to the formulation we have now, where there
+-- are two layers of `Expr`:
+--
+--   from
+--     x m a = a * (1 + (m (x m a)))
+--     m = fix x
+--   we derive
+--     x (fix x) a = a * (1 + (fix x (x (fix x) a)))
+--     fix x a = a * (1 + (fix x (fix x a)))
+--     m a = a * (1 + (m (m a)))
+--
+-- When we use that kind of two-layer representation, we lose the
+-- important ability to set a different initial value of `m`. Setting
+-- a different `m` lets us apply this higher quasiquotation design
+-- starting from different concrete syntaxes.
+
+data CallList a = CallList a (Maybe (CallList a))
+  deriving Show
+
+instance Functor CallList where
+  fmap func (CallList x xs) = CallList (func x) (fmap (fmap func) xs)
+
+data CallSExpr a = CallSExpr a (Maybe (CallList (CallSExpr a)))
+  deriving Show
+
+instance Functor CallSExpr where
+  fmap func (CallSExpr op body) =
+    CallSExpr (func op) (fmap (fmap (fmap func)) body)
+
+newtype ExprAsCallFunctor m a =
+  ExprAsCallFunctor {
+    deExprAsCallFunctor :: (Expr (Compose Maybe m) a Void)
+  }
+
+callListToExpr :: CallList a -> ExprAsCallFunctor Identity a
+callListToExpr (CallList x xs) =
+  ExprAsCallFunctor $ Call x $ Literal $ Compose $
+    fmap (Identity . deExprAsCallFunctor . callListToExpr) xs
+
+callListFromExpr ::
+  ExprAsCallFunctor Identity a -> CallList (Either String a)
+callListFromExpr (ExprAsCallFunctor expr) = case expr of
+  Literal void -> absurd void
+  Call x body -> case body of
+    Call op' body' ->
+      CallList
+        (Left "Encountered content nested within a call-list element")
+        Nothing
+    Literal body' -> CallList (Right x) $
+      fmap (callListFromExpr . ExprAsCallFunctor . runIdentity) $
+      getCompose body'
+
+callSExprToExpr ::
+  CallSExpr a -> ExprAsCallFunctor (ExprAsCallFunctor Identity) a
+callSExprToExpr (CallSExpr a list) =
+  ExprAsCallFunctor $ Call a $ Literal $ Compose $
+    fmap
+      (callListToExpr . fmap (deExprAsCallFunctor . callSExprToExpr))
+      list
+
+callSExprFromExpr ::
+  ExprAsCallFunctor (ExprAsCallFunctor Identity) a ->
+    CallSExpr (Either String a)
+callSExprFromExpr (ExprAsCallFunctor expr) = case expr of
+  Literal void -> absurd void
+  Call a body -> case body of
+    Call op' body' ->
+      CallSExpr
+        (Left "Encountered content nested within a call-s-expression layer's contour")
+        Nothing
+    Literal body' ->
+      CallSExpr (Right a) $ fmap (fmap recur . callListFromExpr) $
+        getCompose body'
+        where
+          recur ::
+            Either String
+             (Expr (Compose Maybe (ExprAsCallFunctor Identity))
+               a Void) ->
+             CallSExpr (Either String a)
+          recur errorExpr = case errorExpr of
+            Left error -> CallSExpr (Left error) Nothing
+            Right expr -> callSExprFromExpr $ ExprAsCallFunctor expr
+
+showShownListToExpr :: [String] -> String
+showShownListToExpr list =
+  "(ExprAsFunctor " ++
+    (case list of
+      [] -> "(Literal ())"
+      x:xs ->
+        "(Call " ++ x ++ " $ Literal $ Just $ " ++
+          "deExprAsFunctor " ++ showShownListToExpr xs ++ ")"
+    ) ++ ")"
+
+showListToExpr :: (Show a) => [a] -> String
+showListToExpr = showShownListToExpr . fmap show
+
+showSExprToExpr :: (Show a) => SExpr a -> String
+showSExprToExpr sExpr =
+  "(ExprAsFunctor " ++
+    (case sExpr of
+      SExprLiteral a ->
+        "(Call (Just " ++ show a ++ ") $ Literal $ " ++
+          "ExprAsFunctor $ Literal ())"
+      SExprList list ->
+        "(Call Nothing $ Literal " ++
+          showShownListToExpr
+            (fmap
+              (\elem ->
+                "(deExprAsFunctor " ++ showSExprToExpr elem ++ ")")
+              list) ++ ")"
+    ) ++ ")"
+
+showShownCallListToExpr :: CallList String -> String
+showShownCallListToExpr (CallList x xs) =
+  "(ExprAsCallFunctor $ Call " ++ x ++ " $ Literal $ " ++
+    "Compose " ++
+    (case xs of
+      Nothing -> "Nothing"
+      Just xs' ->
+        "$ Just $ Identity $ deExprAsCallFunctor " ++
+          showShownCallListToExpr xs'
+    ) ++ ")"
+
+showCallListToExpr :: (Show a) => CallList a -> String
+showCallListToExpr = showShownCallListToExpr . showList
+  where
+    showList (CallList x xs) = CallList (show x) (fmap showList xs)
+
+showCallSExprToExpr :: (Show a) => CallSExpr a -> String
+showCallSExprToExpr (CallSExpr a list) =
+  "(ExprAsCallFunctor $ Call " ++ show a ++ " $ Literal $ " ++
+    "Compose " ++
+    (case list of
+      Nothing -> "Nothing"
+      Just list' ->
+        "$ Just " ++
+          showShownCallListToExpr
+            (fmap
+              (\expr ->
+                "(deExprAsCallFunctor " ++
+                  showCallSExprToExpr expr ++ ")")
+              list')
+    ) ++ ")"
+
+-- listFromExpr $ listToExpr [1, 2, 3, 4]
+-- showSExprToExpr $ SExprList [SExprList [SExprLiteral 1], SExprList [SExprLiteral 2]]
+-- sExprFromExpr $ sExprToExpr $ SExprList [SExprList [SExprLiteral 1], SExprList [SExprLiteral 2]]
+-- showCallListToExpr $ CallList 1 $ Just $ CallList 2 $ Just $ CallList 3 Nothing
+-- callListFromExpr $ callListToExpr $ CallList 1 $ Just $ CallList 2 $ Just $ CallList 3 Nothing
+-- showCallSExprToExpr $ CallSExpr 1 (Just $ CallList (CallSExpr 2 Nothing) $ Just $ CallList (CallSExpr 3 Nothing) Nothing)
+-- callSExprFromExpr $ callSExprToExpr $ CallSExpr 1 (Just $ CallList (CallSExpr 2 Nothing) $ Just $ CallList (CallSExpr 3 Nothing) Nothing)
